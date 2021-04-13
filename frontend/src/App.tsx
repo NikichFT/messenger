8d97a300386af951496a86137fbd63393164a826
Maxim Sinyukov <verero1173@yandex.ru>
2021-04-13 12:23:41 +0300

    feat: установить typescript и sass

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -7,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+         HelloTypeScript!
         </p>
         <a
           className="App-link"
